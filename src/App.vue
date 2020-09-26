4fc7ef407f90f260dd08d146f8e3aec052a659ad
Will Lane <williamlane923@gmail.com>
2020-09-26 15:38:12 -0700

    adding all files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="$store.dispatch('increment')">Hello</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,8 +18,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    points: 0
   },
   mutations: {
+    increment (state) {
+      if (localStorage.getItem('points') === null) {
+        localStorage.setItem('points', state.points)
+        console.log(state.points)
+      } else {
+        state.points = localStorage.getItem('points')
+        state.points++
+        localStorage.setItem('points', state.points)
+        console.log(state.points)
+      }
+    }
   },
   actions: {
+    increment(context) {
+      context.commit('increment')
+    }
   },
   modules: {
   }
